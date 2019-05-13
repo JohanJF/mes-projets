f9d1eac2adcb248c37e87823ef1d4c2e2d7e5f43
Johan <jeanfrancois.johan@stagiairesifa.fr>
2019-05-13 16:55:15 +0200

    ajout fenêtre modal + replacement input titre

--- ifamaker/js/main.js
+++ ifamaker/js/main.js
@@ -1,11 +1,13 @@
 
 /* déclaration de l'id unique de ma table */
 
-var id_table = 0;
-var updated = false;
+var id_table = 0; // id unique pour chaque table
+var updated = false; // variable servant à la modification du titre de la table
 
 function creer_table()
 {
+
+	//----------------------    ETAPE 1    -----------------------------------------------
 
 	/* création de mes noeuds constituant une table */
 
@@ -44,6 +46,7 @@ function creer_table()
 
 	var liste_input_carte = document.createElement("li");
 		liste_input_carte.className = "list-group-item";
+		liste_input_carte.id = 'li-'+id_table; // id unique du li qui contient balise input (titre , cf : fonction supprimer_input)
 
 
 	var div_input_carte = document.createElement("div");
@@ -54,7 +57,7 @@ function creer_table()
 		input_carte.className = "form-control";
 		input_carte.placeholder = "Tache";
 		input_carte.id = 'titre_tache-' + id_table;
-		input_carte.setAttribute('onkeypress','if (event.keyCode == 13) creer_tache("'+liste_carte.id+'","'+input_carte.id+'")');
+		input_carte.setAttribute('onkeypress','if (event.keyCode == 13) creer_tache("'+liste_carte.id+'","'+input_carte.id+'","'+liste_input_carte.id+'")');
 
 
 	var div_bouton_carte = document.createElement("div");
@@ -64,7 +67,7 @@ function creer_table()
 	var bouton_carte_creer = document.createElement("button");
 		bouton_carte_creer.className = "btn btn-outline-secondary";
 		bouton_carte_creer.type = "button";
-		bouton_carte_creer.setAttribute("onclick","creer_tache('"+liste_carte.id+"','"+input_carte.id+"')");
+		bouton_carte_creer.setAttribute("onclick","creer_tache('"+liste_carte.id+"','"+input_carte.id+"','"+liste_input_carte.id+"')");
 
 		console.log(liste_carte.id);
 
@@ -111,10 +114,17 @@ function creer_table()
 		/* on ajoute le contenu entier dans la div "ma_base" */
 		document.getElementById("ma_base").appendChild(mon_article);
 
+
+		//----------------------    ETAPE 2    -----------------------------------------------
+
+
 		/* On supprime la table qui nou permet de créer une nouvelle table */
+
 		supprimer_table('nouvelle_table');  
 
-		////////////////////////////////////////////////////////////////////////
+
+		//----------------------    ETAPE 3    -----------------------------------------------
+
 
 		/* on crée à la suite une nouvelle table permettant la creation d'une table */
 
@@ -172,7 +182,7 @@ function creer_table()
 		document.getElementById("ma_base").appendChild(mon_article_creation);
 
 		ma_tache = 0;
-		id_table++;		
+		id_table++;	// incrémentation de l'id unique de la table	
 
 }
 
@@ -191,8 +201,29 @@ function supprimer_table(id_table)
 
 /* création d'une tache dans une liste */
 
-function creer_tache(id_liste,id_input)
+//var id_tache_increment = 0; // id unique pour chaque tâche
+
+function creer_tache(id_liste,id_input,id_li_input)
 {
+
+	//----------------------    ETAPE 1    -----------------------------------------------
+
+	/* création de la tache visible dans table */
+
+	var id_tache_increment = 0; // id unique pour chaque tâche
+	var nb_tache = document.getElementById(id_liste).getElementsByTagName('li');
+	id_tache_increment = nb_tache.length; 			// nombre de li dans une liste 
+	id_tache = id_liste + '-' + id_tache_increment; // id unique pour chaque tâche li (ex : ma_liste-0-0) en utilisant id de la liste
+	console.log(id_tache);
+
+
+	id_tache = id_liste + '-' + id_tache_increment; // id unique pour chaque tâche li (ex : ma_liste-0-0) en utilisant id de la liste
+
+	var lien_modal = document.createElement('a');
+		lien_modal.className = 'tache';
+		lien_modal.href = '#' + id_tache;
+		lien_modal.setAttribute('data-toggle','modal');
+
 	var ma_tache_liste = document.createElement("li");
 		ma_tache_liste.className = "list-group-item tache_detail";
 
@@ -200,10 +231,13 @@ function creer_tache(id_liste,id_input)
 	
 	titre_tache_element = document.createTextNode(document.getElementById(id_input).value);
 
+	lien_modal.appendChild(ma_tache_liste);
 	ma_tache_liste.appendChild(titre_tache);
 	titre_tache.appendChild(titre_tache_element);
 
-	document.getElementById(id_liste).appendChild(ma_tache_liste);
+	document.getElementById(id_liste).appendChild(lien_modal);
+
+
 
 	/*
 		// nombre de li dans une liste 
@@ -216,9 +250,217 @@ function creer_tache(id_liste,id_input)
 	}
 	*/
 
-	console.log(id_liste);
-	console.log(id_input);
+	//----------------------    ETAPE 2    -----------------------------------------------
+
+	/* création de la fenêtre modale liée à la tâche visible lors du click */
+
+	// modal header
+
+		var modal_fade = document.createElement('div');
+			modal_fade.className = 'modal fade';
+			modal_fade.id = id_tache;
+			modal_fade.setAttribute('tabindex','-1');
+			modal_fade.setAttribute('role','dialog');
+			modal_fade.setAttribute('aria-hidden','true');
+			modal_fade.setAttribute('aria-labelledby',id_tache+'Label');
+
+		var modal_dialog = document.createElement('div');
+			modal_dialog.className = 'modal-dialog modal-dialog-centered';
+			modal_dialog.setAttribute('role','document');
+
+		var modal_content = document.createElement('div');
+			modal_content.className = 'modal-content';
+
+		var modal_header = document.createElement('div');
+			modal_header.className = 'modal-header';
+
+		var modal_title = document.createElement('h5'); // PS ajouter createTexteNode
+			modal_title.className = 'modal-title';
+
+		var modal_title_text = document.createTextNode(document.getElementById(id_input).value);
+
+		var button_close = document.createElement('button');
+			button_close.className = 'close';
+			button_close.type = 'button';
+			button_close.setAttribute('data-dismiss','modal');
+			button_close.setAttribute('aria-label','close');
+
+		var span = document.createElement('span');
+			span.setAttribute('aria-hidden', 'true');
+
+		var span_text = document.createTextNode('Fermer'); // texte
+
+		// modal body 
+
+		var modal_body = document.createElement('div');
+			modal_body.className = 'modal-body';
+
+		var form_group_modal = document.createElement('div');
+			form_group_modal.className = 'form-group';
+
+		var container_modal = document.createElement('div');
+			container_modal.className = 'container';
+
+	/*	var row_modal = document.createElement('div');
+			row_modal.className = 'row';
+
+		var col_modal = document.createElement('div');
+			col_modal.className = 'col';
+
+		var small = document.createElement('small'); // PS ajouter createTexteNode
+			small.className = 'col';
+
+		var button_modifier_modal = document.createElement('button');
+			button_modifier_modal.className = 'btn btn-info btn-sm';
+
+		var button_modifier_modal_text = document.createTextNode('modifier'); // texte
+
+		var button_supprimer_modal = document.createElement('button');
+			button_supprimer_modal.className = 'btn btn-danger btn-sm';
+
+		var button_supprimer_modal_text = document.createTextNode('supprimer'); //texte */
+
+		var label = document.createElement('label');
+			label.setAttribute('for','comment');
+			label.className = 'text-left'
+
+		var label_text = document.createTextNode('A faire:'); // texte
+
+		var textarea = document.createElement('textarea');
+			textarea.className = 'form-control';
+			textarea.rows = '5';
+			textarea.id ='comment';
+
+		// modal footer
+
+		var modal_footer = document.createElement('div');
+			modal_footer.className = 'modal-footer';
+
+		var button_annuler_modal = document.createElement('button');
+			button_annuler_modal.className = 'btn btn-secondary';
+			button_annuler_modal.type = 'button';
+			button_annuler_modal.setAttribute('data-dismiss','modal');
+
+		var button_annuler_modal_text = document.createTextNode('Annuler');
+
+		var button_sauvegarder_modal = document.createElement('button');
+			button_sauvegarder_modal.className ='btn btn-primary';
+			button_sauvegarder_modal.type = 'button';
+
+
+		var button_sauvegarder_modal_text = document.createTextNode('Sauvegarder'); // texte
+
+		modal_fade.appendChild(modal_dialog);
+		modal_dialog.appendChild(modal_content);
+		modal_content.appendChild(modal_header);
+		modal_header.appendChild(modal_title);
+		modal_title.appendChild(modal_title_text);
+		modal_header.appendChild(button_close);
+		button_close.appendChild(span);
+		span.appendChild(span_text);
+		modal_content.appendChild(modal_body)
+		modal_body.appendChild(form_group_modal);
+		form_group_modal.appendChild(container_modal);
+	/*	container_modal.appendChild(row_modal);
+		row_modal.appendChild(col_modal);
+		col_modal.appendChild(small);
+		col_modal.appendChild(button_modifier_modal);
+		button_modifier_modal.appendChild(button_modifier_modal_text);
+		col_modal.appendChild(button_supprimer_modal);
+		button_supprimer_modal.appendChild(button_supprimer_modal_text); */
+		form_group_modal.appendChild(label);
+		label.appendChild(label_text);
+		form_group_modal.appendChild(textarea);
+		modal_content.appendChild(modal_footer);
+		modal_footer.appendChild(button_annuler_modal)
+		button_annuler_modal.appendChild(button_annuler_modal_text);
+		modal_footer.appendChild(button_sauvegarder_modal);
+		button_sauvegarder_modal.appendChild(button_sauvegarder_modal_text);
+
+
+
+		document.getElementById('fenetre_modal').appendChild(modal_fade);
+
+
+		//----------------------    ETAPE 3    -----------------------------------------------	
+
+
+		/* supprimer input liée à la liste */
+
+		supprimer_input(id_li_input);
+
+
+		//----------------------    ETAPE 4    -----------------------------------------------
+
+		/* création d'un nouvel input */
+
+		var liste_input_carte = document.createElement("li");
+		liste_input_carte.className = "list-group-item";
+		liste_input_carte.id = id_li_input;
+
+
+		var div_input_carte = document.createElement("div");
+			div_input_carte.className = "input-group input-group-sm";
+
+		var input_carte = document.createElement("input");
+			input_carte.type = "text";
+			input_carte.className = "form-control";
+			input_carte.placeholder = "Tache";
+			input_carte.id = id_input;
+			input_carte.setAttribute('onkeypress','if (event.keyCode == 13) creer_tache("'+id_liste+'","'+id_input+'","'+id_li_input+'")');
+
+
+		var div_bouton_carte = document.createElement("div");
+			div_bouton_carte.className = "input-group-append";
+
+
+		var bouton_carte_creer = document.createElement("button");
+			bouton_carte_creer.className = "btn btn-outline-secondary";
+			bouton_carte_creer.type = "button";
+			bouton_carte_creer.setAttribute("onclick","creer_tache('"+id_liste+"','"+id_input+"','"+id_li_input+"')");
+
+			bouton_carte_creer_element = document.createTextNode("Créer");
+
+		liste_input_carte.appendChild(div_input_carte);
+		div_input_carte.appendChild(input_carte);
+		div_input_carte.appendChild(div_bouton_carte);
+		div_bouton_carte.appendChild(bouton_carte_creer);
+		bouton_carte_creer.appendChild(bouton_carte_creer_element);
+
+		document.getElementById(id_liste).appendChild(liste_input_carte);
+
+
+
+
+	//-------------------------------------------------------------------------------------------
+
+	
+	// nombre de li dans une liste 
+
+	//var nb_tache = document.getElementById(id_liste).getElementsByTagName('li');
+
+	//id_tache_increment = nb_tache.length + 1;
+	
+	
+
+	//id_tache_increment++; // incrémentation de id_tache
+
+	//console.log(lien_modal.href);
 }
+
+
+/* suppression de li input */
+
+function supprimer_input(id_input)
+{
+	var li = document.getElementById(id_input);
+	if (li.parentNode) 
+	{
+		li.parentNode.removeChild(li);
+	}
+}
+
+
 
 
 /* crée un input dans la balise "titre" */
@@ -287,6 +529,8 @@ function modification_texte(id_titre,titre_modif)
 	mon_titre.innerHTML = document.getElementById(titre_modif).value;
 }
 
+
+
 /* Crée un élément "input texte" afin de modifier les informations personnelles de l'user */
 
 function modifier_info(id)
@@ -336,139 +580,6 @@ function modifier_info(id)
 	
 }
 
-/* cree une fenêtre modal sur tache */
-
-function creer()
-{
-	/* modal header */
-
-	var modal_fade = document.createElement('div');
-		modal_fade.className = 'modal fade';
-		modal_fade.id = 'moi';
-		modal_fade.setAttribute('tabindex','-1');
-		modal_fade.setAttribute('role','dialog');
-		modal_fade.setAttribute('aria-hidden','true');
-		modal_fade.setAttribute('aria-labelledby','moiLabel');
-
-	var modal_dialog = document.createElement('div');
-		modal_dialog.className = 'modal-dialog modal-dialog-centered';
-		modal_dialog.setAttribute('role','document');
-
-	var modal_content = document.createElement('div');
-		modal_content.className = 'modal-content';
-
-	var modal_header = document.createElement('div');
-		modal_header.className = 'modal-header';
-
-	var modal_title = document.createElement('h5'); // PS ajouter createTexteNode
-		modal_title.className = 'modal-title';
-
-	var button_close = document.createElement('button');
-		button_close.className = 'close';
-		button_close.type = 'button';
-		button_close.setAttribute('data-dismiss','modal');
-		button_close.setAttribute('aria-label','close');
-
-	var span = document.createElement('span');
-		span.setAttribute('aria-hidden', 'true');
-
-	var span_text = document.createTextNode('fermer'); // texte
-
-	/* modal body */
-
-	var modal_body = document.createElement('div');
-		modal_body.className = 'modal-body';
-
-	var form_group_modal = document.createElement('div');
-		form_group_modal.className = 'form-group';
-
-	var container_modal = document.createElement('div');
-		container_modal.className = 'container';
-
-/*	var row_modal = document.createElement('div');
-		row_modal.className = 'row';
-
-	var col_modal = document.createElement('div');
-		col_modal.className = 'col';
-
-	var small = document.createElement('small'); // PS ajouter createTexteNode
-		small.className = 'col';
-
-	var button_modifier_modal = document.createElement('button');
-		button_modifier_modal.className = 'btn btn-info btn-sm';
-
-	var button_modifier_modal_text = document.createTextNode('modifier'); // texte
-
-	var button_supprimer_modal = document.createElement('button');
-		button_supprimer_modal.className = 'btn btn-danger btn-sm';
-
-	var button_supprimer_modal_text = document.createTextNode('supprimer'); //texte */
-
-	var label = document.createElement('label');
-		label.setAttribute('for','comment');
-		label.className = 'text-left'
-
-	var label_text = document.createTextNode('A faire:'); // texte
-
-	var textarea = document.createElement('textarea');
-		textarea.className = 'form-control';
-		textarea.rows = '5';
-		textarea.id ='comment';
-
-	/* modal footer */
-
-	var modal_footer = document.createElement('div');
-		modal_footer.className = 'modal-footer';
-
-	var button_annuler_modal = document.createElement('button');
-		button_annuler_modal.className = 'btn btn-secondary';
-		button_annuler_modal.type = 'button';
-		button_annuler_modal.setAttribute('data-dismiss','modal');
-
-	var button_annuler_modal_text = document.createTextNode('Annuler');
-
-	var button_sauvegarder_modal = document.createElement('button');
-		button_sauvegarder_modal.className ='btn btn-primary';
-		button_sauvegarder_modal.type = 'button';
-
-
-	var button_sauvegarder_modal_text = document.createTextNode('Sauvegarder'); // texte
-
-	modal_fade.appendChild(modal_dialog);
-	modal_dialog.appendChild(modal_content);
-	modal_content.appendChild(modal_header);
-	modal_header.appendChild(modal_title);
-	modal_header.appendChild(button_close);
-	button_close.appendChild(span);
-	span.appendChild(span_text);
-	modal_content.appendChild(modal_body)
-	modal_body.appendChild(form_group_modal);
-	form_group_modal.appendChild(container_modal);
-/*	container_modal.appendChild(row_modal);
-	row_modal.appendChild(col_modal);
-	col_modal.appendChild(small);
-	col_modal.appendChild(button_modifier_modal);
-	button_modifier_modal.appendChild(button_modifier_modal_text);
-	col_modal.appendChild(button_supprimer_modal);
-	button_supprimer_modal.appendChild(button_supprimer_modal_text); */
-	form_group_modal.appendChild(label);
-	label.appendChild(label_text);
-	form_group_modal.appendChild(textarea);
-	modal_content.appendChild(modal_footer);
-	modal_footer.appendChild(button_annuler_modal)
-	button_annuler_modal.appendChild(button_annuler_modal_text);
-	modal_footer.appendChild(button_sauvegarder_modal);
-	button_sauvegarder_modal.appendChild(button_sauvegarder_modal_text);
-
-
-
-	document.getElementById('hello').appendChild(modal_fade);
-
-
-
-
-
-}
 
 
 
