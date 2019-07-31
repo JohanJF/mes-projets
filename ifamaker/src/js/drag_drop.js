cc14fb9b1b3286e08446f68f4114ba9c7ad35a00
Johan <jeanfrancois.johan@stagiairesifa.fr>
2019-07-31 16:39:24 +0200

    update drag & drop

--- ifamaker/src/js/drag_drop.js
+++ ifamaker/src/js/drag_drop.js
@@ -1,4 +1,5 @@
 /* Récupère l'id dans l'URL */
+
 function $_GET(param) {
 	var vars = {};
 	window.location.href.replace( location.hash, '' ).replace( 
@@ -14,44 +15,8 @@ function $_GET(param) {
 	return vars;
 }
 
-/*$('.ma_zone').sortable({
-	items : $('.card-liste'),
-	connectWith : $('.card-liste'),
-	revert : true,
-	cursor : "move",
-	placeholder : 'bg_PlaceHolder',
-	forcePlaceholderSize : true,
-	stop: function(event, ui){
-        // Pour chaque item de liste
-        $('.ma_zone').find(".card-liste").each(function(){
-            // On actualise sa position
-            index = parseInt($(this).index()+1);
-            console.log(index);
-            // On la met à jour dans la page
-            $(this).find(".count").text(index);
-        });
-    },
-     update: function()
-    {
-        // On prépare la variable contenant les paramètres
-        var order = $(this).sortable("serialize")+'&action=updateListeOrder';
-        console.log(order);
-        // $(this).sortable("serialize") sera le paramètre "element", un tableau contenant les différents "id"
-        // action sera le paramètre qui permet éventuellement par la suite de gérer d'autres scripts de mise à jour
- 
-        // Ensuite on appelle notre page updateListe.php en lui passant en paramètre la variable order
-        $.post("./src/AJAX/drag_list.php",order, {id_tableau : $_GET('id')}, function(theResponse)
-        {
-        	console.log(theResponse);
-            // On affiche dans l'élément portant la classe "reponse" le résultat du script de mise à jour
-            $(".reponse").html(theResponse).fadeIn("fast");
-            setTimeout(function()
-            {
-                $(".reponse").fadeOut("slow");
-            }, 2000);
-        });
-    }
-})*/
+
+/* UPDATE BDD Drag & Drop */
 
 $('.ma_zone').sortable({
 	items : $('.card-liste'),
@@ -62,36 +27,39 @@ $('.ma_zone').sortable({
 	forcePlaceholderSize : true,
 	start: function(event, ui) {
         // Create a temporary attribute on the element with the old index
-        $(this).attr('data-currentindex', ui.item.index());
-        console.log($(this).attr('data-currentindex'));
-    }/*,
+        $(this).attr('data-currentindex', ui.item.index()); // implémente dans un attribut la position de liste
+    },
     update: function(event, ui) {
-        let user_id = $('#user_id').val();
-        let current_position = $(this).attr('data-currentindex');
-        let desired_position = ui.item.index();
+        let id = ui.item.attr('id'); // id de la liste 
+        let current_position = parseInt($(this).attr('data-currentindex'))+1; // position actuelle de la liste 
+        let desired_position = ui.item.index()+1; // position d'arrivé de la liste
+        console.log('id item : ' + id);
+        console.log('position desirée : ' + desired_position);
+        console.log('position actuelle : ' + current_position);
 
-        // Reset the current index
+        /*// Reset the current index
         $(this).removeAttr('data-currentindex');
 
         // Post to the server to handle the changes
         $.ajax({
             type: "POST",
-            url: "/url-to-handle-database-updates/",
+            url: "./src/AJAX/drag_list.php",
             data: {
                 desired_position: desired_position,
                 current_position: current_position,
-                user_id: user_id
+                id: id
             },
             beforeSend: function() {
                 // Disable dragging
-                $('.todos').sortable('disable');
+                $('.ma_zone').sortable('disable');
             },
             success: function(html) {
                 // Re-enable dragging
-                $('.todos').sortable('enable');
+                $('.ma_zone').sortable('enable');
+                console.log(html);
             }
-        });
-    }*/
+        });*/
+    }
 })
 
 $('.groupe_tache').sortable({
@@ -99,5 +67,52 @@ $('.groupe_tache').sortable({
 	revert : true,
 	cursor : "move",
 	placeholder : 'bg-IFA',
-	forcePlaceholderSize : true
+	forcePlaceholderSize : true,
+	start: function(event, ui) {
+        // Create a temporary attribute on the element with the old index
+        let id_liste_depart = ui.item.parent().attr('id'); 
+        	id_liste_depart = id_liste_depart.split('-'); // transforme l'id de la liste de départ en tableau
+        $(this).attr('data-currentindexParent',id_liste_depart[1]); // implémente dans un attribut l'id de la liste
+        $(this).attr('data-currentindex', ui.item.index()); // implémente dans un attribut la position de la tache
+    },
+    stop: function(event, ui) {
+        let id_task = ui.item.attr('id'); 
+         	id_task = id_task.split('-'); // transforme l'id de la tache en tableau
+        let id_liste = ui.item.parent().attr('id'); 
+        	id_liste = id_liste.split('-'); // transforme l'id de la liste en tableau
+        let current_position = $(this).attr('data-currentindex'); // position actuelle de la tache
+        let desired_position = ui.item.index(); // position d'arrivé de la tache
+
+        console.log($(this).attr('data-currentindexParent'));
+        console.log('id_tableau final : ' + id_liste[1]);
+        console.log('id item : ' + id_task[1]);
+        console.log('position desirée : ' + desired_position);
+        console.log('position actuelle : ' + current_position);
+
+       /* // Reset the current index
+        $(this).removeAttr('data-currentindex');
+
+        // Post to the server to handle the changes
+        $.ajax({
+            type: "POST",
+            url: "./src/AJAX/drag_list.php",
+            data: {
+                desired_position: desired_position,
+                current_position: current_position,
+                id: id
+            },
+            beforeSend: function() {
+                // Disable dragging
+                $('.ma_zone').sortable('disable');
+            },
+            success: function(html) {
+                // Re-enable dragging
+                $('.ma_zone').sortable('enable');
+                console.log(html);
+            }
+        });*/
+    }
 })
+
+
+
